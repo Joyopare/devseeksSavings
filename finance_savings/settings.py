4d3edd5fb7b36345cf9260af893d12a687d148eb
cloud99 <oparejoy05@gmail.com>
2026-05-22 12:09:49 +0000

    updated

--- finance_savings/settings.py
+++ finance_savings/settings.py
@@ -122,12 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # ← this is what's missing
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Media files (for profile pictures)
