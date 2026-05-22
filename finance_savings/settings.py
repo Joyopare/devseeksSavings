991287a7c51d3fbc91953e49136d50368e5bfbab
cloud99 <oparejoy05@gmail.com>
2026-05-22 12:27:35 +0000

    updated

--- finance_savings/settings.py
+++ finance_savings/settings.py
@@ -135,8 +135,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Login/Logout URLs
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
